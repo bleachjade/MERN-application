7595ffac67a3ecff08dd8ffede3e3ea666451956
bleachjade <you@example.combleachjade@hotmail.com>
2022-01-15 21:16:11 +0700

    basic CRUD MERN app memories

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://merndatabase:merndatabase123@cluster0.zk9ei.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongodb
